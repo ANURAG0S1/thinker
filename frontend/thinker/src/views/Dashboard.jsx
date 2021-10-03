2363e4b2dcd37710f14461148594ab62822b1f61
Anurag <anuragkush051.dev@gmail.com>
2021-10-03 15:25:03 +0530

    ..

--- frontend/thinker/src/views/Dashboard.jsx
+++ frontend/thinker/src/views/Dashboard.jsx
@@ -22,6 +22,23 @@ function Dashboard() {
 
 
             </div>
+
+            <section style={{ alignItems: "center", display: "flex", fontFamily: "poppins", justifyContent: "center", textAlign: "center", marginTop: "50px" }}>
+                <div>
+                    <h1 style={{
+                        fontSize: "32px",
+                        lineHeight: "30px",
+                        fontWeight: "500"
+                    }}>
+                        We will help your idea and stories to reach thousand others like you
+                    </h1>
+                    <br />
+                    <h3 style={{ fontWeight: "400", textAlign: "center", width: "100%" }}>
+                        expand your imagination and let us know what you can think of
+                    </h3>
+                    <button className="button" style={{ marginTop: "30px", fontWeight: "bolder" }}>Join Us</button>
+                </div>
+            </section>
         </>
     )
 }
